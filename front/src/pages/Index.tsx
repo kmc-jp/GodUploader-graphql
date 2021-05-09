049293cbbb7ee9291994e65e184423aa657325f2
utgwkk <utagawakiki@gmail.com>
2021-05-09 21:55:41 +0900

    front: index page component

--- front/src/pages/Index.tsx
+++ front/src/pages/Index.tsx
@@ -1,7 +1,8 @@
-import { graphql } from 'react-relay';
-import React from 'react';
+import { graphql, usePreloadedQuery, PreloadedQuery } from "react-relay";
+import React from "react";
+import type { IndexQuery } from "./__generated__/IndexQuery.graphql";
 
-export const IndexQuery = graphql`
+export const indexQuery = graphql`
   query IndexQuery {
     accounts(sort: [FOLDERS_COUNT_DESC]) {
       edges {
@@ -32,4 +33,51 @@ export const IndexQuery = graphql`
   }
 `;
 
-export const Index: React.VFC = () => <h2>It works!</h2>
+interface Props {
+  queryReference: PreloadedQuery<IndexQuery, Record<string, unknown>>;
+}
+
+export const Index: React.VFC<Props> = ({ queryReference }: Props) => {
+  const { safeArtworks, accounts } = usePreloadedQuery<IndexQuery>(
+    indexQuery,
+    queryReference
+  );
+  const artworkCount = safeArtworks?.edges?.length || 0;
+
+  return (
+    <div>
+      <div>
+        <h2>最新{artworkCount}件の絵</h2>
+        {safeArtworks?.edges.map((edge) => {
+          if (!edge) {
+            return null;
+          }
+          const node = edge.node!;
+          const firstIllust = node.illusts!.edges![0]!.node!;
+          const account = node.account!;
+
+          return (
+            <div>
+              <img src={firstIllust.filename} alt={node?.title} />
+              <div className="caption">
+                <h3>{node?.title}</h3>
+                <p>{account.name}</p>
+                <p>{node?.caption}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <h2>利用者達</h2>
+        {accounts?.edges.map((edge) => {
+          if (!edge) {
+            return null;
+          }
+          const node = edge.node!;
+          return <div>{node.name}({node.foldersCount})</div>
+        })}
+      </div>
+    </div>
+  );
+};
