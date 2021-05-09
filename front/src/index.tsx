e617675f06f6f85a9aa3ef11a8a0abcfd86311e3
utgwkk <utagawakiki@gmail.com>
2021-05-09 21:31:19 +0900

    front: suspense

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import RelayEnvironment from "./RelayEnvironment";
+import { RelayEnvironmentProvider } from "react-relay/hooks";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RelayEnvironmentProvider environment={RelayEnvironment}>
+      <Suspense fallback={<p>Now loading...</p>}>
+        <App />
+      </Suspense>
+    </RelayEnvironmentProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
