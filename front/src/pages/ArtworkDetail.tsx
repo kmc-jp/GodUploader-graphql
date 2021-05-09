9f13d4879a54793c5bac37a7c9aa600ebaadb814
utgwkk <utagawakiki@gmail.com>
2021-05-10 08:32:17 +0900

    front: artwork detail page

---
+++ front/src/pages/ArtworkDetail.tsx
@@ -0,0 +1,81 @@
+import React from "react";
+import { PreloadedQuery, usePreloadedQuery } from "react-relay";
+import { graphql } from "babel-plugin-relay/macro";
+import { ArtworkDetailQuery } from "./__generated__/ArtworkDetailQuery.graphql";
+
+export const artworkDetailQuery = graphql`
+  query ArtworkDetailQuery($id: ID!) {
+    node(id: $id) {
+      ... on Artwork {
+        title
+        caption
+        createdAt
+        illusts {
+          edges {
+            node {
+              id
+              filename
+            }
+          }
+        }
+        likes {
+          edges {
+            node {
+              account {
+                id
+                kmcid
+              }
+            }
+          }
+        }
+        tags {
+          edges {
+            node {
+              tag {
+                id
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+interface ArtworkDetailProps {
+  prepared?: {
+    artworkDetailQuery: PreloadedQuery<ArtworkDetailQuery, Record<string, any>>;
+  };
+}
+
+export const ArtworkDetail: React.FC<ArtworkDetailProps> = ({ prepared }) => {
+  const { node: artwork } = usePreloadedQuery<ArtworkDetailQuery>(
+    artworkDetailQuery,
+    prepared!.artworkDetailQuery
+  );
+  if (!artwork) {
+    return <div>作品が見つかりません</div>;
+  }
+
+  return (
+    <div>
+      <h2>{artwork.title}</h2>
+      <p>{artwork.caption}</p>
+      <p>{artwork.createdAt as any}</p>
+      {artwork.illusts?.edges.map((edge) => {
+        if (!edge) {
+          return null;
+        }
+        const node = edge.node;
+        return (
+          <img
+            src={`http://localhost:5000/public/illusts/${node?.filename}`}
+            key={node?.id}
+            alt=""
+          />
+        );
+      })}
+    </div>
+  );
+};
---
+++ front/src/pages/__generated__/ArtworkDetailQuery.graphql.ts
@@ -0,0 +1,439 @@
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from "relay-runtime";
+export type ArtworkDetailQueryVariables = {
+    id: string;
+};
+export type ArtworkDetailQueryResponse = {
+    readonly node: {
+        readonly title?: string;
+        readonly caption?: string;
+        readonly createdAt?: unknown;
+        readonly illusts?: {
+            readonly edges: ReadonlyArray<{
+                readonly node: {
+                    readonly id: string;
+                    readonly filename: string;
+                } | null;
+            } | null>;
+        } | null;
+        readonly likes?: {
+            readonly edges: ReadonlyArray<{
+                readonly node: {
+                    readonly account: {
+                        readonly id: string;
+                        readonly kmcid: string;
+                    } | null;
+                } | null;
+            } | null>;
+        } | null;
+        readonly tags?: {
+            readonly edges: ReadonlyArray<{
+                readonly node: {
+                    readonly tag: {
+                        readonly id: string;
+                        readonly name: string;
+                    } | null;
+                } | null;
+            } | null>;
+        } | null;
+    } | null;
+};
+export type ArtworkDetailQuery = {
+    readonly response: ArtworkDetailQueryResponse;
+    readonly variables: ArtworkDetailQueryVariables;
+};
+
+
+
+/*
+query ArtworkDetailQuery(
+  $id: ID!
+) {
+  node(id: $id) {
+    __typename
+    ... on Artwork {
+      title
+      caption
+      createdAt
+      illusts {
+        edges {
+          node {
+            id
+            filename
+          }
+        }
+      }
+      likes {
+        edges {
+          node {
+            account {
+              id
+              kmcid
+            }
+            id
+          }
+        }
+      }
+      tags {
+        edges {
+          node {
+            tag {
+              id
+              name
+            }
+            id
+          }
+        }
+      }
+    }
+    id
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "title",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "caption",
+  "storageKey": null
+},
+v4 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "createdAt",
+  "storageKey": null
+},
+v5 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v6 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "IllustConnection",
+  "kind": "LinkedField",
+  "name": "illusts",
+  "plural": false,
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "IllustEdge",
+      "kind": "LinkedField",
+      "name": "edges",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "Illust",
+          "kind": "LinkedField",
+          "name": "node",
+          "plural": false,
+          "selections": [
+            (v5/*: any*/),
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "filename",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+},
+v7 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "Account",
+  "kind": "LinkedField",
+  "name": "account",
+  "plural": false,
+  "selections": [
+    (v5/*: any*/),
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "kmcid",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+},
+v8 = {
+  "alias": null,
+  "args": null,
+  "concreteType": "Tag",
+  "kind": "LinkedField",
+  "name": "tag",
+  "plural": false,
+  "selections": [
+    (v5/*: any*/),
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "name",
+      "storageKey": null
+    }
+  ],
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ArtworkDetailQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              (v2/*: any*/),
+              (v3/*: any*/),
+              (v4/*: any*/),
+              (v6/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "LikeConnection",
+                "kind": "LinkedField",
+                "name": "likes",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "LikeEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Like",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v7/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "ArtworkTagRelationConnection",
+                "kind": "LinkedField",
+                "name": "tags",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "ArtworkTagRelationEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "ArtworkTagRelation",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v8/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "type": "Artwork",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "ArtworkDetailQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "node",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "__typename",
+            "storageKey": null
+          },
+          (v5/*: any*/),
+          {
+            "kind": "InlineFragment",
+            "selections": [
+              (v2/*: any*/),
+              (v3/*: any*/),
+              (v4/*: any*/),
+              (v6/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "LikeConnection",
+                "kind": "LinkedField",
+                "name": "likes",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "LikeEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Like",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v7/*: any*/),
+                          (v5/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "ArtworkTagRelationConnection",
+                "kind": "LinkedField",
+                "name": "tags",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "ArtworkTagRelationEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "ArtworkTagRelation",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v8/*: any*/),
+                          (v5/*: any*/)
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "type": "Artwork",
+            "abstractKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "401abc814d527f9a6484ddbbca28f202",
+    "id": null,
+    "metadata": {},
+    "name": "ArtworkDetailQuery",
+    "operationKind": "query",
+    "text": "query ArtworkDetailQuery(\n  $id: ID!\n) {\n  node(id: $id) {\n    __typename\n    ... on Artwork {\n      title\n      caption\n      createdAt\n      illusts {\n        edges {\n          node {\n            id\n            filename\n          }\n        }\n      }\n      likes {\n        edges {\n          node {\n            account {\n              id\n              kmcid\n            }\n            id\n          }\n        }\n      }\n      tags {\n        edges {\n          node {\n            tag {\n              id\n              name\n            }\n            id\n          }\n        }\n      }\n    }\n    id\n  }\n}\n"
+  }
+};
+})();
+(node as any).hash = '8bac9d502f3e94cc9722878bdc7daaf6';
+export default node;
