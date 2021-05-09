434e51d4876160135bc21f976023306355b03a8f
utgwkk <utagawakiki@gmail.com>
2021-05-09 21:29:10 +0900

    front: fix babel-plugin-relay/macro issue

--- front/src/react-app-env.d.ts
+++ front/src/react-app-env.d.ts
@@ -1,1 +1,5 @@
 /// <reference types="react-scripts" />
+// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35707
+declare module 'babel-plugin-relay/macro' {
+	export { graphql } from 'react-relay'
+}
