6841a402bcc269b08d2c434b04b143929a40a340
utgwkk <utagawakiki@gmail.com>
2021-05-09 20:08:12 +0900

    api: LikeArtwork mutation

--- api/src/goduploader/schema.py
+++ api/src/goduploader/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 from sqlalchemy.sql.elements import or_, not_
+from db import session
 from model import (
     Account as AccountModel,
     ArtworkTagRelation as ArtworkTagRelationModel,
@@ -83,4 +84,33 @@ class Query(graphene.ObjectType):
 
         return artworks
 
-schema = graphene.Schema(query=Query)
+class LikeArtwork(relay.ClientIDMutation):
+    class Input:
+        artwork_id = graphene.ID(required=True)
+
+    like = graphene.Field(Like)
+
+    @classmethod
+    def mutate_and_get_payload(cls, root, info, **input):
+        current_user = viewer()
+        if current_user is None:
+            raise Exception('Please login')
+
+        artwork_id = input['artwork_id']
+        artwork = relay.Node.get_node_from_global_id(info, artwork_id, only_type=Artwork)
+        if artwork is None:
+            raise Exception('Artwork not found')
+
+        like = LikeModel(
+            account_id=current_user.id,
+            artwork_id=artwork.id,
+        )
+        session.add(like)
+        session.commit()
+
+        return LikeArtwork(like=like)
+
+class Mutation(graphene.ObjectType):
+    like_artwork = LikeArtwork.Field()
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
