301bdd6d32a896499a19f97da9b172aadddbef62
utgwkk <utagawakiki@gmail.com>
2021-05-11 07:54:32 +0900

    api: find_or_create

--- api/src/goduploader/viewer.py
+++ api/src/goduploader/viewer.py
@@ -4,4 +4,19 @@ from db import session
 from typing import Optional
 
 def viewer() -> Optional[Account]:
-    return session.query(Account).filter(Account.kmcid == request.headers.get('X-Forwarded-User')).first()
+    kmcid = request.headers.get('X-Forwarded-User')
+    if not kmcid:
+        return unknown_user()
+
+    account = session.query(Account).filter(Account.kmcid == kmcid).first()
+    if account:
+        return account
+
+    with session.begin():
+        account = Account(kmcid=kmcid, name=kmcid)
+        session.new(account)
+
+    return account
+
+def unknown_user():
+    return session.query(Account).filter(Account.kmcid == 'unknown_user').first()
