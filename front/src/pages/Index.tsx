a55d782e291e61332a9588decbbfa8090983fdce
utgwkk <utagawakiki@gmail.com>
2021-06-12 04:35:44 +0900

    front: fix key

--- front/src/pages/Index.tsx
+++ front/src/pages/Index.tsx
@@ -1,7 +1,7 @@
 import { PreloadedQuery, usePreloadedQuery } from "react-relay";
 import { graphql } from "babel-plugin-relay/macro";
 import React from "react";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import type { IndexQuery } from "./__generated__/IndexQuery.graphql";
 import { ArtworkListItem } from "../components/ArtworkListItem";
 import { Chip, GridList, GridListTile, Typography } from "@material-ui/core";
@@ -44,19 +44,27 @@ export const Index: React.VFC<IndexProps> = ({ prepared }) => {
   return (
     <div>
       <div>
-        <Typography component="h2" variant="h6">最新{artworkCount}件の絵</Typography>
+        <Typography component="h2" variant="h6">
+          最新{artworkCount}件の絵
+        </Typography>
         <GridList cols={4} cellHeight="auto">
           {safeArtworks?.edges.map((edge, i) => {
             if (!edge) {
               return null;
             }
 
-            return <GridListTile><ArtworkListItem artwork={edge.node!} key={i}/></GridListTile>
+            return (
+              <GridListTile key={i}>
+                <ArtworkListItem artwork={edge.node!} />
+              </GridListTile>
+            );
           })}
         </GridList>
       </div>
       <div>
-        <Typography component="h2" variant="h6">利用者達</Typography>
+        <Typography component="h2" variant="h6">
+          利用者達
+        </Typography>
         {activeAccounts?.edges.map((edge) => {
           if (!edge) {
             return null;
@@ -64,7 +72,9 @@ export const Index: React.VFC<IndexProps> = ({ prepared }) => {
           const node = edge.node!;
           return (
             <div key={`accounts-${node.id}`}>
-              <Link to={`/user/${node.kmcid}`}>{node.name}({node.foldersCount})</Link>
+              <Link to={`/user/${node.kmcid}`}>
+                {node.name}({node.foldersCount})
+              </Link>
             </div>
           );
         })}
