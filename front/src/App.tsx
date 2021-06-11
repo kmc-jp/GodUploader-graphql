99724bdc93237e75baaf31cb5e430f66cb240e87
utgwkk <utagawakiki@gmail.com>
2021-06-12 05:48:41 +0900

    front: navbar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,19 @@
 import React, { Suspense } from "react";
+import { Link } from "react-router-dom";
+
 import { RouteConfigComponentProps, RouteRenderer } from "./routing";
 import { ErrorBoundary } from "./errorBoundary";
 
 function App({ route }: RouteConfigComponentProps) {
   return (
     <div className="App">
-      <h1>God Illust Uploader</h1>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            KMC画像アップローダー God Illust Uploader
+          </Link>
+        </div>
+      </nav>
       <div className="container">
         <Suspense fallback={<p>Now loading...</p>}>
           <ErrorBoundary>
