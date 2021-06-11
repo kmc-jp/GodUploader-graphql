37e53e670e5eb259a229cb9c16ee41dfa74ed93c
utgwkk <utagawakiki@gmail.com>
2021-06-12 08:32:25 +0900

    front: navbar links

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -8,10 +8,22 @@ function App({ route }: RouteConfigComponentProps) {
   return (
     <div className="App">
       <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
-        <div className="container-fluid">
+        <div className="container">
           <Link className="navbar-brand" to="/">
             KMC画像アップローダー God Illust Uploader
           </Link>
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link to="/artwork/new" className="nav-link">
+                アップロード
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/my" className="nav-link">
+                マイページ
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
       <div className="container">
