8db87b1048c31fddb8e16fef89af270cc3cc1fd0
utgwkk <utagawakiki@gmail.com>
2021-06-12 07:17:40 +0900

    front: bootstrap (upload form)

--- front/src/pages/UploadArtwork.tsx
+++ front/src/pages/UploadArtwork.tsx
@@ -1,14 +1,18 @@
 import React, { FormEvent, useCallback, useRef, useState } from "react";
 import { useRelayEnvironment } from "react-relay";
 import { Redirect } from "react-router";
-import { commitUploadArtworkMutation, makeUploadables } from "../mutation/UploadArtwork";
+import {
+  commitUploadArtworkMutation,
+  makeUploadables,
+} from "../mutation/UploadArtwork";
 
 export const UploadArtwork: React.VFC = () => {
   const environment = useRelayEnvironment();
   const titleRef = useRef<HTMLInputElement>(null);
   const captionRef = useRef<HTMLInputElement>(null);
   const filesRef = useRef<HTMLInputElement>(null);
-  const [uploadedArtworkId, setUploadedArtworkId] = useState<string | null>(null);
+  const [uploadedArtworkId, setUploadedArtworkId] =
+    useState<string | null>(null);
 
   const handleSubmit = useCallback(
     (event: FormEvent) => {
@@ -19,7 +23,12 @@ export const UploadArtwork: React.VFC = () => {
       const uploadables = makeUploadables(files);
       commitUploadArtworkMutation(
         environment,
-        { title, caption, tags: "", files: uploadables },
+        {
+          title,
+          caption,
+          tags: "", // TODO: タグ設定を受け付ける
+          files: uploadables,
+        },
         uploadables,
         (resp) => {
           setUploadedArtworkId(resp.uploadArtwork!.artwork!.id);
@@ -30,21 +39,60 @@ export const UploadArtwork: React.VFC = () => {
   );
 
   if (uploadedArtworkId) {
-    return <Redirect to={{
-      pathname: `/artwork/${uploadedArtworkId}`,
-    }} />
+    return (
+      <Redirect
+        to={{
+          pathname: `/artwork/${uploadedArtworkId}`,
+        }}
+      />
+    );
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        タイトル: <input type="text" id="title" ref={titleRef} required />
+    <div className="card">
+      <div className="card-header">
+        <p>画像のアップロード</p>
       </div>
-      <div>
-        説明: <input type="text" id="caption" ref={captionRef} />
+      <div className="panel-body">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-sm-12">
+              <label htmlFor="title">タイトル(必須)</label>
+              <input
+                type="text"
+                id="title"
+                className="form-control"
+                ref={titleRef}
+                required
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-sm-12">
+              <label htmlFor="caption">キャプション</label>
+              <input
+                type="text"
+                id="caption"
+                className="form-control"
+                ref={captionRef}
+              />
+            </div>
+          </div>
+          <input
+            type="file"
+            id="file"
+            ref={filesRef}
+            multiple
+            accept="image/*"
+            required
+          />
+          <input
+            type="submit"
+            className="btn btn-primary"
+            value="アップロードする"
+          />
+        </form>
       </div>
-      <input type="file" id="file" multiple ref={filesRef} required />
-      <input type="submit" value="アップロードする" />
-    </form>
+    </div>
   );
 };
