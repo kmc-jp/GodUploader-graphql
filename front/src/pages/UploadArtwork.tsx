cfb5a861a6d88c26b16c9537b389185c6790cc72
utgwkk <utagawakiki@gmail.com>
2021-06-12 09:21:18 +0900

    front: upload form

--- front/src/pages/UploadArtwork.tsx
+++ front/src/pages/UploadArtwork.tsx
@@ -14,9 +14,12 @@ export const UploadArtwork: React.VFC = () => {
   const [uploadedArtworkId, setUploadedArtworkId] =
     useState<string | null>(null);
 
+  const [isUploading, setIsUploading] = useState(false);
+
   const handleSubmit = useCallback(
     (event: FormEvent) => {
       event.preventDefault();
+      setIsUploading(true);
       const title = titleRef.current!.value;
       const caption = captionRef.current!.value;
       const files = filesRef.current!.files!;
@@ -31,8 +34,9 @@ export const UploadArtwork: React.VFC = () => {
         },
         uploadables,
         (resp) => {
+          setIsUploading(false);
           setUploadedArtworkId(resp.uploadArtwork!.artwork!.id);
-        }
+        },
       );
     },
     [environment]
@@ -50,47 +54,60 @@ export const UploadArtwork: React.VFC = () => {
 
   return (
     <div className="card">
-      <div className="card-header">
-        <p>画像のアップロード</p>
-      </div>
-      <div className="panel-body">
+      <div className="card-header">画像のアップロード</div>
+      <div className="panel-body px-4 py-4">
         <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-sm-12">
-              <label htmlFor="title">タイトル(必須)</label>
-              <input
-                type="text"
-                id="title"
-                className="form-control"
-                ref={titleRef}
-                required
-              />
-            </div>
+          <div className="mb-3">
+            <label htmlFor="file" className="form-label">
+              アップロードする画像 <span className="text-danger">(必須)</span>
+            </label>
+            <input
+              type="file"
+              className="form-control"
+              id="file"
+              ref={filesRef}
+              multiple
+              accept="image/*"
+              required
+            />
           </div>
-          <div className="row">
-            <div className="col-sm-12">
-              <label htmlFor="caption">キャプション</label>
-              <input
-                type="text"
-                id="caption"
-                className="form-control"
-                ref={captionRef}
-              />
-            </div>
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              タイトル <span className="text-danger">(必須)</span>
+            </label>
+            <input
+              type="text"
+              id="title"
+              className="form-control"
+              ref={titleRef}
+              required
+            />
           </div>
-          <input
-            type="file"
-            id="file"
-            ref={filesRef}
-            multiple
-            accept="image/*"
-            required
-          />
-          <input
-            type="submit"
-            className="btn btn-primary"
-            value="アップロードする"
-          />
+          <div className="mb-3">
+            <label htmlFor="caption" className="form-label">
+              キャプション
+            </label>
+            <input
+              type="text"
+              id="caption"
+              className="form-control"
+              ref={captionRef}
+            />
+          </div>
+          <div>
+            <input
+              type="submit"
+              className="btn btn-primary form-control"
+              value="アップロードする"
+            />
+          </div>
+          {isUploading && (
+            <div className="mt-3 d-flex justify-content-center">
+              <div className="spinner-border text-primary" role="status">
+                <span className="visually-hidden">Uploading...</span>
+              </div>
+            </div>
+          )}
         </form>
       </div>
     </div>
