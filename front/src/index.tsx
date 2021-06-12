827f52b537279e10357aa410da86de8e7b820459
utgwkk <utagawakiki@gmail.com>
2021-06-12 09:47:39 +0900

    front: import bootstrap-icons

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 import RelayEnvironment from "./RelayEnvironment";
 import { RelayEnvironmentProvider } from "react-relay/hooks";
