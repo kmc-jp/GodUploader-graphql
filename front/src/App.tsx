659fe9799639d8b79222f8dbbd4a16ab6093c4f2
utgwkk <utagawakiki@gmail.com>
2021-06-13 23:48:51 +0900

    front: collapse navigation bar

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -24,7 +24,7 @@ function App({ route }: RouteConfigComponentProps) {
   useEffect(() => {
     const dispose = history.listen((location, action) => {
       if (location !== currentLocation) {
-        if (action !== 'REPLACE') {
+        if (action !== "REPLACE") {
           previousLocation.current = currentLocation;
         }
         setIsLoading(true);
@@ -45,28 +45,41 @@ function App({ route }: RouteConfigComponentProps) {
   return (
     <div className="App">
       {isLoading ? <LoadingOverlay /> : null}
-      <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+      <nav className="navbar navbar-expand-xl navbar-light bg-light mb-3">
         <div className="container">
           <Link className="navbar-brand" to="/">
             KMC画像アップローダー God Illust Uploader
           </Link>
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link to="/artwork/new" className="nav-link">
-                アップロード
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/my" className="nav-link">
-                マイページ
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/tags" className="nav-link">
-                タグ検索
-              </Link>
-            </li>
-          </ul>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link to="/artwork/new" className="nav-link">
+                  アップロード
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/my" className="nav-link">
+                  マイページ
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/tags" className="nav-link">
+                  タグ検索
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <div className="container">
