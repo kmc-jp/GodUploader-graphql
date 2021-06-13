c280c70096b7fdba3a8dc9163f66611b380a9774
utgwkk <utagawakiki@gmail.com>
2021-06-13 21:05:21 +0900

    front: avoid image suspended

--- front/src/components/ArtworkListItem.tsx
+++ front/src/components/ArtworkListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { graphql } from "babel-plugin-relay/macro";
 import { useFragment } from "react-relay/hooks";
 import { Link } from "react-router-dom";
@@ -35,22 +35,29 @@ export const ArtworkListItem: React.VFC<ArtworkListItemProps> = (props) => {
   const account = artwork.account;
 
   return (
-    <div className="card p-1" style={{height: 320}}>
-      <SuspenseImage
-        src={`http://localhost:5000/public/thumbnail/${firstIllust.filename}`}
-        alt={artwork.title}
-        style={{
-          maxWidth: "100%",
-          height: 186,
-          objectFit: "contain",
-          display: "block",
-          filter: artwork.nsfw ? 'blur(20px)' : undefined,
-        }}
-        className="card-img-top mx-auto my-0"
-      />
+    <div className="card p-1" style={{ height: 320 }}>
+      <Suspense fallback={null /* TODO: show spinner or placeholder image */}>
+        <SuspenseImage
+          src={`http://localhost:5000/public/thumbnail/${firstIllust.filename}`}
+          alt={artwork.title}
+          style={{
+            maxWidth: "100%",
+            height: 186,
+            objectFit: "contain",
+            display: "block",
+            filter: artwork.nsfw ? "blur(20px)" : undefined,
+          }}
+          className="card-img-top mx-auto my-0"
+        />
+      </Suspense>
       <div className="card-body">
         <h3 className="text-truncate">
-          <Link to={`/artwork/${artwork.id}`} className="card-link text-body stretched-link">{artwork.title}</Link>
+          <Link
+            to={`/artwork/${artwork.id}`}
+            className="card-link text-body stretched-link"
+          >
+            {artwork.title}
+          </Link>
         </h3>
         <p className="card-text text-truncate">{account!.name}</p>
         <p className="card-text text-truncate">{artwork.caption}</p>
