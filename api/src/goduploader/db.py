c054a53a9cbc491b35b750aa73fe13df377a2d12
utgwkk <utagawakiki@gmail.com>
2021-06-14 22:36:08 +0900

    api: config from env

--- api/src/goduploader/db.py
+++ api/src/goduploader/db.py
@@ -1,8 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///god.db', echo=True)
+db_url = os.environ.get('DB_URL', 'sqlite:///god.db')
+engine = create_engine(db_url, echo=True)
 Session = sessionmaker(bind=engine)
 session = scoped_session(Session)
 
--- api/src/main.py
+++ api/src/main.py
@@ -1,9 +1,10 @@
+import os
 from flask import Flask
 from flask_graphql import GraphQLView
 from flask_cors import CORS
 from goduploader.db import session
 from goduploader.schema import schema
-app = Flask(__name__, static_folder='../public')
+app = Flask(__name__, static_folder=os.environ.get('PUBLIC_FOLDER', '../public'))
 app.debug = True
 CORS(app)
 
