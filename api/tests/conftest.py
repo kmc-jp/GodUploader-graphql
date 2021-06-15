52980ebac18845330e9fad09ba0c219245000b8e
utgwkk <utagawakiki@gmail.com>
2021-06-15 21:40:21 +0900

    api: prepare temporary public directory for test

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,9 +1,22 @@
+import os
+from pathlib import Path
+import tempfile
 import pytest
 from tests.util import create_account
 from graphene.test import Client
 from goduploader.db import engine
 from goduploader.model import Base
 from goduploader.graphql.schema import schema
+
+def prepare_temporary_public_dir():
+    public = Path(tempfile.mkdtemp())
+    illusts_dir = public / 'illusts'
+    illusts_dir.mkdir()
+    thumbnail_dir = public / 'thumbnail'
+    thumbnail_dir.mkdir()
+    os.environ['PUBLIC_FOLDER'] = str(public)
+
+prepare_temporary_public_dir()
 
 @pytest.fixture(scope='function', autouse=True)
 def client():
