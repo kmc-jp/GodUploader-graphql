64f6e96bc068af40f54326268595fc84619fca2e
utgwkk <utagawakiki@gmail.com>
2021-06-15 14:45:14 +0900

    api: fix import path

--- api/tests/prepare.py
+++ api/tests/prepare.py
@@ -3,6 +3,7 @@ from graphene.test import Client
 from goduploader.db import engine
 from goduploader.model import Base
 from goduploader.graphql.schema import schema
+from .util import prepare_fixtures
 
 @pytest.fixture(scope='session')
 def client():
