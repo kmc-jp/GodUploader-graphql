6930bada675bed1445eef81ab6323a34148cd70d
utgwkk <utagawakiki@gmail.com>
2021-06-16 07:47:21 +0900

    api: test taggedArtworks query

--- api/tests/graphql/test_query.py
+++ api/tests/graphql/test_query.py
@@ -32,3 +32,41 @@ def test_safe_artworks(client):
             },
         },
     }, 'only fetch safe artworks'
+
+def test_tagged_artworks(client):
+    artworks = [
+        create_artwork(),
+        create_artwork(tags=['foo']),
+        create_artwork(tags=['bar', 'foo']),
+        create_artwork(tags=['bar']),
+    ]
+    query = '''
+    {
+        taggedArtworks(tag: "foo", sort: ID_ASC) {
+            edges {
+                node {
+                    title
+                }
+            }
+        }
+    }
+    '''
+    result = client.execute(query)
+    assert result == {
+        'data': {
+            'taggedArtworks': {
+                'edges': [
+                    {
+                        'node': {
+                            'title': artworks[1].title
+                        }
+                    },
+                    {
+                        'node': {
+                            'title': artworks[2].title
+                        }
+                    },
+                ],
+            },
+        },
+    }
