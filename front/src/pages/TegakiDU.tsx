a3a272e83fb925314a91bf1adc97dc6f12766db2
utgwkk <utagawakiki@gmail.com>
2021-06-17 09:30:13 +0900

    front: color suggestion

--- front/src/pages/TegakiDU.tsx
+++ front/src/pages/TegakiDU.tsx
@@ -129,6 +129,29 @@ const Canvas: React.VFC<{ width: number; height: number }> = ({
   );
 };
 
+const ColorSuggestion: React.VFC = () => {
+  const colorsGrid = [
+    ["#000", "#fff", "#a80515", "#f1d0d0", "#4b3d38", "#eae5d5"],
+    ["#384b43", "#cfeadd", "#313768", "#d5e9f3", "#800000", "#f0e0d6"],
+  ];
+  return (
+    <div>
+      <p>↓おすすめの色ですカラーピッカー使ってください</p>
+      {colorsGrid.map((colors, i) => (
+        <div key={i} className="d-flex justify-content-around">
+          {colors.map((color, j) => (
+            <div
+              key={j}
+              className="p-2 d-inline-block border border-dark"
+              style={{ width: 64, height: 64, backgroundColor: color }}
+            ></div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+};
+
 const Sidebar: React.VFC = () => {
   const {
     color,
@@ -196,6 +219,7 @@ export const TegakiDU: React.VFC = () => {
               </div>
             </DrawingProvider>
           </div>
+          <ColorSuggestion />
         </div>
       </div>
     </div>
