327929bd268efe0c4eb530fad52cba71c8056f84
utgwkk <utagawakiki@gmail.com>
2021-06-19 18:34:32 +0900

    api: use variables instead of template string literal

--- api/tests/graphql/test_mutation.py
+++ api/tests/graphql/test_mutation.py
@@ -3,32 +3,38 @@ from tests.util import create_account, create_artwork, mock_context
 from goduploader.db import session
 from graphene.relay import Node
 
+
 def test_update_artwork_can_owner_only(client):
     account_1 = create_account()
     account_2 = create_account()
 
     artwork = create_artwork(account=account_1)
 
-    mutation = f'''
-    mutation {{
-        updateArtwork(input: {{
-            id: "{Node.to_global_id('Artwork', artwork.id)}",
+    mutation = """
+    mutation UpdateArtworkTestMutation ($id: ID!) {
+        updateArtwork(input: {
+            id: $id,
             title: "new title",
             caption: "new caption",
             tags: ["new tag"],
-        }}) {{
-            artwork {{
+        }) {
+            artwork {
                 id
-            }}
-        }}
-    }}
-    '''
-    result = client.execute(mutation, context_value=mock_context(kmcid=account_2.kmcid))
-    assert result['errors'][0]['message'] == 'You cannot update this artwork'
+            }
+        }
+    }
+    """
+    result = client.execute(
+        mutation,
+        variable_values={"id": Node.to_global_id("Artwork", artwork.id)},
+        context_value=mock_context(kmcid=account_2.kmcid),
+    )
+    assert result["errors"][0]["message"] == "You cannot update this artwork"
 
     after_artwork = session.query(Artwork).filter_by(id=artwork.id).first()
     assert after_artwork.title == artwork.title
     assert after_artwork.caption == artwork.caption
+
 
 def test_delete_artwork_can_owner_only(client):
     account_1 = create_account()
@@ -36,38 +42,45 @@ def test_delete_artwork_can_owner_only(client):
 
     artwork = create_artwork(account=account_1)
 
-    mutation = f'''
-    mutation {{
-        deleteArtwork(input: {{
-            id: "{Node.to_global_id('Artwork', artwork.id)}",
-        }}) {{
+    mutation = """
+    mutation DeleteArtworkTestMutation ($id: ID!) {
+        deleteArtwork(input: {id: $id}) {
             deletedArtworkId
-        }}
-    }}
-    '''
-    result = client.execute(mutation, context_value=mock_context(kmcid=account_2.kmcid))
-    assert result['errors'][0]['message'] == 'You cannot delete this artwork'
+        }
+    }
+    """
+    result = client.execute(
+        mutation,
+        variable_values={"id": Node.to_global_id("Artwork", artwork.id)},
+        context_value=mock_context(kmcid=account_2.kmcid),
+    )
+    assert result["errors"][0]["message"] == "You cannot delete this artwork"
 
     after_artwork = session.query(Artwork).filter_by(id=artwork.id).first()
     assert after_artwork is not None
+
 
 def test_update_account(client):
     account_1 = create_account()
     account_2 = create_account()
 
-    mutation = f'''
-    mutation {{
-        updateAccount(input: {{
-            name: "new name"
-        }}) {{
-            account {{
+    mutation = """
+    mutation UpdateAccountTestMutation($name: String!) {
+        updateAccount(input: {name: $name}) {
+            account {
                 id
-            }}
-        }}
-    }}
-    '''
-    result = client.execute(mutation, context_value=mock_context(kmcid=account_1.kmcid))
-    assert 'errors' not in result
+            }
+        }
+    }
+    """
+    result = client.execute(
+        mutation,
+        variable_values={
+            "name": "new name",
+        },
+        context_value=mock_context(kmcid=account_1.kmcid),
+    )
+    assert "errors" not in result
 
     after_account_1 = session.query(Account).filter_by(id=account_1.id).first()
     after_account_2 = session.query(Account).filter_by(id=account_2.id).first()
