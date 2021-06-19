58dec60641cf3cd5c4214ec5b7ad787f0c1b34f6
utgwkk <utagawakiki@gmail.com>
2021-06-20 01:07:49 +0900

    front: show color code

--- front/src/pages/TegakiDU/components/ColorSuggestion.tsx
+++ front/src/pages/TegakiDU/components/ColorSuggestion.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const ColorSuggestion: React.VFC = () => {
   const colorsGrid = [
-    ["#000", "#fff", "#a80515", "#f1d0d0", "#4b3d38", "#eae5d5"],
+    ["#000000", "#ffffff", "#a80515", "#f1d0d0", "#4b3d38", "#eae5d5"],
     ["#384b43", "#cfeadd", "#313768", "#d5e9f3", "#800000", "#f0e0d6"],
   ];
   return (
@@ -11,11 +11,14 @@ export const ColorSuggestion: React.VFC = () => {
       {colorsGrid.map((colors, i) => (
         <div key={i} className="d-flex justify-content-around">
           {colors.map((color, j) => (
-            <div
-              key={j}
-              className="p-2 d-inline-block border border-dark"
-              style={{ width: 64, height: 64, backgroundColor: color }}
-            ></div>
+            <div>
+              <div
+                key={j}
+                className="p-2 d-inline-block border border-dark"
+                style={{ width: 64, height: 64, backgroundColor: color }}
+              ></div>
+              <div>{color}</div>
+            </div>
           ))}
         </div>
       ))}
