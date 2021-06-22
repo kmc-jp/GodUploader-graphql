bcad3ccf6dacfb7c65181c57f1921f11f33e17aa
utgwkk <utagawakiki@gmail.com>
2021-06-22 20:06:07 +0900

    api: require BASE

--- api/script/init.py
+++ api/script/init.py
@@ -2,19 +2,20 @@ import os
 from pathlib import Path
 import subprocess
 
-host = os.environ['HOST']
-base_dir = os.environ['BASE_DIR']
+base = os.environ["BASE"]
 
 cwd = Path(__file__).parent.parent
-local_db_path = cwd / 'god.db'
+local_db_path = cwd / "god.db"
 local_db_path.unlink(missing_ok=True)
+
 
 def run(args):
     subprocess.run(args, cwd=cwd)
 
-run(['rsync', '-auvz', f'{host}:{base_dir}/god.db', './from.db'])
-run(['rsync', '-auvz', f'{host}:{base_dir}/public/illusts/', 'public/illusts/'])
-run(['rsync', '-auvz', f'{host}:{base_dir}/public/thumbnail/', 'public/thumbnail/'])
 
-run(['poetry', 'run', 'python', 'script/create_schema.py'])
-run(['poetry', 'run', 'python', 'script/migrate.py'])
+run(["rsync", "-auvz", f"{base}/god.db", "./from.db"])
+run(["rsync", "-auvz", f"{base}/public/illusts/", "public/illusts/"])
+run(["rsync", "-auvz", f"{base}/public/thumbnail/", "public/thumbnail/"])
+
+run(["poetry", "run", "python", "script/create_schema.py"])
+run(["poetry", "run", "python", "script/migrate.py"])
