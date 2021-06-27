de6c114f1ff456665ffb0d030b74723ce9d2be37
utgwkk <utagawakiki@gmail.com>
2021-06-27 09:48:00 +0900

    api: add deleteArtwork mutation test

--- api/tests/graphql/test_delete_artwork.py
+++ api/tests/graphql/test_delete_artwork.py
@@ -1,6 +1,6 @@
-from goduploader.model import Artwork
+from goduploader.model import Account, Artwork, Tag
 from tests.util import create_account, create_artwork, mock_context
-from goduploader.db import session
+from goduploader.db import session, engine
 from graphene.relay import Node
 
 
@@ -11,6 +11,35 @@ mutation DeleteArtworkTestMutation ($id: ID!) {
     }
 }
 """
+
+
+def test_delete_artwork(client):
+    account = create_account()
+
+    artwork = create_artwork(account=account, tags=["test"])
+    artwork_2 = create_artwork(tags=["test"])
+
+    after_account = session.query(Account).filter_by(id=account.id).first()
+    assert after_account.artworks_count == 1
+
+    tag = session.query(Tag).filter_by(name="test").first()
+    assert tag.artworks_count == 2
+
+    result = client.execute(
+        DELETE_ARTWORK_QUERY,
+        variable_values={"id": Node.to_global_id("Artwork", artwork.id)},
+        context_value=mock_context(kmcid=account.kmcid),
+    )
+    assert "errors" not in result
+
+    after_artwork = session.query(Artwork).filter_by(id=artwork.id).first()
+    assert after_artwork is None
+
+    after_account = session.query(Account).filter_by(id=account.id).first()
+    assert after_account.artworks_count == 0
+
+    after_tag = session.query(Tag).filter_by(name="test").first()
+    assert after_tag.artworks_count == 1
 
 
 def test_delete_artwork_can_owner_only(client):
