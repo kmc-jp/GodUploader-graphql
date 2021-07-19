b9aeb3de131a9480f0cacf9c971be60badc8c5aa
utgwkk <utagawakiki@gmail.com>
2021-07-20 08:25:54 +0900

    accountモデルを別ファイルに分ける

--- api/src/goduploader/model/__init__.py
+++ api/src/goduploader/model/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from urllib.parse import urljoin
 
 from goduploader.config import app_config
+from goduploader.model.account import Account
 from goduploader.model.base import Base
 from graphene.relay import Node
 from sqlalchemy import Column
@@ -12,32 +13,7 @@ from sqlalchemy.sql.expression import text
 from sqlalchemy.sql.schema import ForeignKey, Index, Table
 from sqlalchemy.sql.sqltypes import Boolean, DateTime, Integer, String, Text
 
-__all__ = ["Base"]
-
-
-class Account(Base):
-    __tablename__ = "account"
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    kmcid = Column(String, unique=True, nullable=False)
-    name = Column(String, nullable=False)
-    artworks_count = Column(Integer, nullable=False, default=0)
-    last_logged_in = Column(DateTime, nullable=False, default=datetime.now)
-
-    created_at = Column(DateTime, nullable=False, default=datetime.now)
-    updated_at = Column(
-        DateTime, nullable=False, default=datetime.now, onupdate=datetime.now
-    )
-
-    @property
-    def user_page_url(self):
-        return urljoin(app_config.base_url, f"users/{self.kmcid}")
-
-    index_artworks_count = Index("account_artworks_count", artworks_count)
-
-    artworks = relationship("Artwork", backref="account")
-    comments = relationship("Comment", backref="account")
-    likes = relationship("Like", backref="account")
+__all__ = ["Account", "Base"]
 
 
 artwork_tag_relation = Table(
