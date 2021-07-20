58c6ce5e54e2201ca44ca17ed4d4d662ac0be409
utgwkk <utagawakiki@gmail.com>
2021-07-20 21:05:51 +0900

    deleteArtwork mutationを別ファイルに分ける

--- api/src/goduploader/graphql/type/mutation/__init__.py
+++ api/src/goduploader/graphql/type/mutation/__init__.py
@@ -2,48 +2,14 @@ import graphene
 import sqlalchemy
 from goduploader.db import session
 from goduploader.graphql.type.account import Account
-from goduploader.graphql.type.artwork import Artwork
 from goduploader.graphql.type.mutation.create_comment import CreateComment
+from goduploader.graphql.type.mutation.delete_artwork import DeleteArtwork
 from goduploader.graphql.type.mutation.like_artwork import LikeArtwork
 from goduploader.graphql.type.mutation.update_artwork import UpdateArtwork
 from goduploader.graphql.type.mutation.upload_artwork import UploadArtwork
 from goduploader.graphql.type.tag import Tag
 from goduploader.model import Tag as TagModel
 from graphene import relay
-
-
-class DeleteArtwork(graphene.ClientIDMutation):
-    class Input:
-        id = graphene.ID(description="削除対象の作品のID", required=True)
-
-    deleted_artwork_id = graphene.Field(graphene.ID)
-
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        current_user = info.context.user
-        if current_user is None:
-            raise Exception("Please login")
-
-        artwork_id = input["id"]
-        artwork = relay.Node.get_node_from_global_id(
-            info, artwork_id, only_type=Artwork
-        )
-        if artwork is None:
-            raise Exception("Artwork not found")
-
-        if artwork.account_id != current_user.id:
-            raise Exception("You cannot delete this artwork")
-
-        for tag in artwork.tags:
-            tag.artworks_count -= 1
-
-        current_user.artworks_count -= 1
-
-        session.delete(artwork)
-
-        session.commit()
-
-        return DeleteArtwork(deleted_artwork_id=artwork_id)
 
 
 class UpdateAccount(graphene.ClientIDMutation):
