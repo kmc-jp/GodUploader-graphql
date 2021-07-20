d30b82a568399be85ca071c196d8c10afae8078f
utgwkk <utagawakiki@gmail.com>
2021-07-20 21:06:36 +0900

    updateAccount mutationを別ファイルに分ける

--- api/src/goduploader/graphql/type/mutation/__init__.py
+++ api/src/goduploader/graphql/type/mutation/__init__.py
@@ -1,34 +1,15 @@
 import graphene
 import sqlalchemy
 from goduploader.db import session
-from goduploader.graphql.type.account import Account
 from goduploader.graphql.type.mutation.create_comment import CreateComment
 from goduploader.graphql.type.mutation.delete_artwork import DeleteArtwork
 from goduploader.graphql.type.mutation.like_artwork import LikeArtwork
+from goduploader.graphql.type.mutation.update_account import UpdateAccount
 from goduploader.graphql.type.mutation.update_artwork import UpdateArtwork
 from goduploader.graphql.type.mutation.upload_artwork import UploadArtwork
 from goduploader.graphql.type.tag import Tag
 from goduploader.model import Tag as TagModel
 from graphene import relay
-
-
-class UpdateAccount(graphene.ClientIDMutation):
-    class Input:
-        name = graphene.String(desciption="更新後の表示名", required=True)
-
-    account = graphene.Field(Account)
-
-    @classmethod
-    def mutate_and_get_payload(cls, root, info, **input):
-        current_user = info.context.user
-        if current_user is None:
-            raise Exception("Please login")
-
-        current_user.name = input["name"]
-
-        session.commit()
-
-        return UpdateAccount(account=current_user)
 
 
 class UpdateTag(graphene.ClientIDMutation):
