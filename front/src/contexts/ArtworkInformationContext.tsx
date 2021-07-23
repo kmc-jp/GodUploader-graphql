01e80a48a252a063aa579939b1b9d00c5ecac90a
utgwkk <utagawakiki@gmail.com>
2021-07-23 15:34:37 +0900

    年齢制限設定をcontextに持つ

--- front/src/contexts/ArtworkInformationContext.tsx
+++ front/src/contexts/ArtworkInformationContext.tsx
@@ -1,19 +1,27 @@
 import React, { createContext, useState } from "react";
 
+type AgeRestriction =
+  | "SAFE" // 全年齢
+  | "R-18"
+  | "R-18G";
+
 export type ArtworkInformationContextValue = {
   title: string;
   caption: string;
   tags: string[];
+  ageRestriction: AgeRestriction;
 
   setTitle: (title: string) => void;
   setCaption: (caption: string) => void;
   setTags: (tags: string[]) => void;
+  setAgeRestriction: (restriction: AgeRestriction) => void;
 };
 
 const defaultInformation: ArtworkInformationContextValue = {
   title: "",
   caption: "",
   tags: [],
+  ageRestriction: "SAFE",
 
   setTitle: () => {
     /* noop */
@@ -22,6 +30,9 @@ const defaultInformation: ArtworkInformationContextValue = {
     /* noop */
   },
   setTags: () => {
+    /* noop */
+  },
+  setAgeRestriction: () => {
     /* noop */
   },
 };
@@ -40,10 +51,21 @@ export const ArtworkInformationProvider: React.FC<ArtworkInformationProviderProp
     const [title, setTitle] = useState<string>(initialTitle ?? "");
     const [caption, setCaption] = useState<string>(initialCaption ?? "");
     const [tags, setTags] = useState<string[]>(initialTags ?? []);
+    const [ageRestriction, setAgeRestriction] =
+      useState<AgeRestriction>("SAFE");
 
     return (
       <ArtworkInformationContext.Provider
-        value={{ title, caption, tags, setTitle, setCaption, setTags }}
+        value={{
+          title,
+          caption,
+          tags,
+          ageRestriction,
+          setTitle,
+          setCaption,
+          setTags,
+          setAgeRestriction,
+        }}
       >
         {children}
       </ArtworkInformationContext.Provider>
