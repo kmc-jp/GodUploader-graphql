d94ee91ab414731ae062254b281c40c906576b6e
utgwkk <utagawakiki@gmail.com>
2021-07-23 15:52:22 +0900

    年齢制限を選択するラジオボックス

---
+++ front/src/components/ArtworkInfoForm/AgeRestrictionInput.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import { useArtworkInformation } from "../../hooks/useArtworkInformation";
+
+export const AgeRestrictionInput: React.VFC = () => {
+  const { ageRestriction, setAgeRestriction } = useArtworkInformation();
+
+  return (
+    <div className="form-group row">
+      <div className="col-sm-1">年齢制限</div>
+      <div className="col-sm-11">
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id="age_restriction_safe"
+            checked={ageRestriction === "SAFE"}
+            onChange={() => setAgeRestriction("SAFE")}
+          />
+          <label className="form-check-label" htmlFor="age_restriction_safe">
+            全年齢
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id="age_restriction_r_18"
+            checked={ageRestriction === "R-18"}
+            onChange={() => setAgeRestriction("R-18")}
+          />
+          <label className="form-check-label" htmlFor="age_restriction_r_18">
+            R-18
+          </label>
+        </div>
+        <div className="form-check form-check-inline">
+          <input
+            className="form-check-input"
+            type="radio"
+            id="age_restriction_r_18g"
+            checked={ageRestriction === "R-18G"}
+            onChange={() => setAgeRestriction("R-18G")}
+          />
+          <label className="form-check-label" htmlFor="age_restriction_r_18g">
+            R-18G
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
