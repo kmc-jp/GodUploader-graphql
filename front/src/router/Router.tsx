4e2b8b0e639b7b8a57c24947b12c62bc9807f0d3
utgwkk <utagawakiki@gmail.com>
2021-08-01 11:40:21 +0900

    暫定的に、5分おきにページ遷移時に再読み込みされるようにする

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
 export const Router: React.FC = ({ children }) => {
+  const [forceRefresh, setForceRefresh] = useState(false);
+
+  // 暫定的に、5分おきにページ遷移時に再読み込みされるようにする
+  useEffect(() => {
+    const timer = window.setTimeout(() => setForceRefresh(true), 5 * 60 * 1000);
+
+    return () => {
+      window.clearTimeout(timer);
+    };
+  }, []);
+
   return (
-    <BrowserRouter basename={process.env.REACT_APP_BASENAME}>
+    <BrowserRouter
+      basename={process.env.REACT_APP_BASENAME}
+      forceRefresh={forceRefresh}
+    >
       {children}
     </BrowserRouter>
   );
