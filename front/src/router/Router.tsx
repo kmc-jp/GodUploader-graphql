ed96e0ce0c757aed4223356bac8b4438c505f508
utgwkk <utagawakiki@gmail.com>
2021-08-01 11:53:47 +0900

    printデバッグ

--- front/src/router/Router.tsx
+++ front/src/router/Router.tsx
@@ -6,7 +6,10 @@ export const Router: React.FC = ({ children }) => {
 
   // 暫定的に、5分おきにページ遷移時に再読み込みされるようにする
   useEffect(() => {
-    const timer = window.setTimeout(() => setForceRefresh(true), 5 * 60 * 1000);
+    const timer = window.setTimeout(() => {
+      console.log("force refresh requested");
+      setForceRefresh(true);
+    }, 5 * 60 * 1000);
 
     return () => {
       window.clearTimeout(timer);
