53540bfd220aba7c545f09ea47a5e0cddbfed539
utgwkk <utagawakiki@gmail.com>
2021-09-25 16:37:04 +0900

    リゾルバを定義する

--- api/src/goduploader/graphql/type/artwork.py
+++ api/src/goduploader/graphql/type/artwork.py
@@ -1,10 +1,12 @@
 import graphene
+from goduploader.db import session
 from goduploader.graphql.dataloader import AccountLoader, IllustLoader
 from goduploader.graphql.type.account import Account
 from goduploader.graphql.type.illust import Illust
 from goduploader.model import Artwork as ArtworkModel
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
+from sqlalchemy.sql.expression import and_, desc
 
 account_loader = AccountLoader()
 illust_loader = IllustLoader()
@@ -30,6 +32,32 @@ class Artwork(SQLAlchemyObjectType):
         lambda: Artwork, description="この作品より1つ新しい、同じユーザーの作品を返す"
     )
 
+    def resolve_next_artwork(root, info):
+        return (
+            session.query(ArtworkModel)
+            .filter(
+                and_(
+                    ArtworkModel.account_id == root.account_id,
+                    ArtworkModel.id > root.id,
+                )
+            )
+            .order_by(ArtworkModel.id)
+            .first()
+        )
+
     previous_artwork = graphene.Field(
         lambda: Artwork, description="この作品より1つ古い、同じユーザーの作品を返す"
     )
+
+    def resolve_previous_artwork(root, info):
+        return (
+            session.query(ArtworkModel)
+            .filter(
+                and_(
+                    ArtworkModel.account_id == root.account_id,
+                    ArtworkModel.id < root.id,
+                )
+            )
+            .order_by(desc(ArtworkModel.id))
+            .first()
+        )
