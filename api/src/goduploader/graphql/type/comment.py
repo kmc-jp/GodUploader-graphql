6a3b6eba6ac9db60dd86079e74ba8161d642f42f
utgwkk <utagawakiki@gmail.com>
2022-03-27 01:39:31 +0900

    ArtworkLoaderを投入

--- api/src/goduploader/graphql/type/comment.py
+++ api/src/goduploader/graphql/type/comment.py
@@ -1,12 +1,14 @@
 import graphene
 from goduploader.db import session
-from goduploader.graphql.dataloader import AccountLoader, IllustLoader
+from goduploader.graphql.dataloader import AccountLoader, ArtworkLoader, IllustLoader
 from goduploader.graphql.type.account import Account
+from goduploader.graphql.type.artwork import Artwork
 from goduploader.model import Comment as CommentModel
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 account_loader = AccountLoader()
+artwork_loader = ArtworkLoader()
 
 
 class Comment(SQLAlchemyObjectType):
@@ -18,3 +20,8 @@ class Comment(SQLAlchemyObjectType):
 
     def resolve_account(root, info):
         return account_loader.load(root.account_id)
+
+    artwork = graphene.Field(Artwork)
+
+    def resolve_artwork(root, info):
+        return artwork_loader.load(root.artwork_id)
--- api/src/goduploader/graphql/type/like.py
+++ api/src/goduploader/graphql/type/like.py
@@ -1,11 +1,13 @@
 import graphene
-from goduploader.graphql.dataloader import AccountLoader, IllustLoader
+from goduploader.graphql.dataloader import AccountLoader, ArtworkLoader
 from goduploader.graphql.type.account import Account
+from goduploader.graphql.type.artwork import Artwork
 from goduploader.model import Like as LikeModel
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType
 
 account_loader = AccountLoader()
+artwork_loader = ArtworkLoader()
 
 
 class Like(SQLAlchemyObjectType):
@@ -17,3 +19,8 @@ class Like(SQLAlchemyObjectType):
 
     def resolve_account(root, info):
         return account_loader.load(root.account_id)
+
+    artwork = graphene.Field(Artwork)
+
+    def resolve_artwork(root, info):
+        return artwork_loader.load(root.artwork_id)
