cc940d92acfe460f17d5a7b526a4301a51e6bbcf
utgwkk <utagawakiki@gmail.com>
2022-04-07 02:45:26 +0900

    MySQLに向けてテスト

--- api/script/init_db.sh
+++ api/script/init_db.sh
@@ -3,4 +3,5 @@ set -eux
 
 MYSQL_HOST=db
 echo 'CREATE DATABASE IF NOT EXISTS goduploader' | mysql -h $MYSQL_HOST
+echo 'CREATE DATABASE IF NOT EXISTS goduploader_test' | mysql -h $MYSQL_HOST
 poetry run alembic upgrade head
