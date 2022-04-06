c33ba3eff18d7b1af419888359ffeddf53ae3b17
utgwkk <utagawakiki@gmail.com>
2022-04-07 02:53:21 +0900

    もろもろ初期化しておく

--- api/script/init_db.sh
+++ api/script/init_db.sh
@@ -5,3 +5,7 @@ MYSQL_HOST=db
 echo 'CREATE DATABASE IF NOT EXISTS goduploader' | mysql -h $MYSQL_HOST
 echo 'CREATE DATABASE IF NOT EXISTS goduploader_test' | mysql -h $MYSQL_HOST
 poetry run alembic upgrade head
+
+poetry run python $(dirname $0)/prepare_unknown_user.py
+poetry run python $(dirname $0)/freeze_edit_for_nsfw_tags.py
+poetry run python $(dirname $0)/freeze_edit_for_tegaki_du.py
