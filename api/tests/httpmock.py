adf4cdcc1ee86a2fcd893fc723e64b1c4855df4e
utgwkk <utagawakiki@gmail.com>
2022-04-07 21:38:46 +0900

    Slackのchat.postMessage APIをモックする

--- api/tests/httpmock.py
+++ api/tests/httpmock.py
@@ -11,7 +11,18 @@ def _mock_gyazo_upload_request():
     )
 
 
+def _mock_slack_api():
+    # https://api.slack.com/methods/chat.postMessage
+    body = (
+        Path(__file__).parent / "data/httpmock/slack_chat_postMessage.json"
+    ).read_text()
+    httpretty.register_uri(
+        httpretty.POST, "https://slack.com/api/chat.postMessage", body=body
+    )
+
+
 def mock_requests():
     httpretty.enable(allow_net_connect=False)
     # モックしたいAPIが増えたらここに追記してください
     _mock_gyazo_upload_request()
+    _mock_slack_api()
