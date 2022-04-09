c802eb1dd42e4f3711eb38a370c522c8a3d22763
utgwkk <utagawakiki@gmail.com>
2022-04-10 00:57:14 +0900

    整理

--- front/next-env.d.ts
+++ front/next-env.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react-scripts" />
 // https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35707
 declare module "babel-plugin-relay/macro" {
   export { graphql } from "react-relay";
