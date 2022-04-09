a91368b319e8ff22115947bfd82ce67d73d8c8ad
utgwkk <utagawakiki@gmail.com>
2022-04-10 03:06:37 +0900

    環境変数なおうす

--- front/components/Footer.tsx
+++ front/components/Footer.tsx
@@ -1,31 +1,34 @@
 import React from "react";
 
+const builtAt = process.env.NEXT_APP_BUILT_AT
+  ? new Date(Number(process.env.NEXT_APP_BUILT_AT) * 1000)
+  : null;
+
+const revision = process.env.NEXT_APP_REVISION;
+
 export const Footer: React.VFC = () => {
-  const builtAt = process.env.REACT_APP_BUILT_AT
-    ? new Date(Number(process.env.REACT_APP_BUILT_AT) * 1000)
-    : null;
-
-  const revision = process.env.REACT_APP_REVISION;
-
   return (
     <footer className="mt-3 py-3">
       <div className="container">
         <div className="text-muted">
-          Goduploader-graphql
-          {builtAt && ` (built at ${builtAt.toLocaleString()})`}
-          {revision && (
-            <>
-              {" "}
-              revision{" "}
-              <a
-                href={`https://github.com/kmc-jp/GodUploader-graphql/commit/${revision}`}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {revision}
-              </a>
-            </>
-          )}
+          <>
+            {`Goduploader-graphql${
+              builtAt ? ` (built at ${builtAt.toLocaleString()})` : ""
+            }`}
+            {revision && (
+              <>
+                {" "}
+                revision{" "}
+                <a
+                  href={`https://github.com/kmc-jp/GodUploader-graphql/commit/${revision}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {revision}
+                </a>
+              </>
+            )}
+          </>
         </div>
       </div>
     </footer>
