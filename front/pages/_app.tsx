3a0d971d78185f2a579df6e404b7d6c5b6397e7d
utgwkk <utagawakiki@gmail.com>
2022-04-10 02:28:16 +0900

    スタイルあたるように

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,3 +1,5 @@
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { AppProps } from "next/app";
 import { ReactRelayContext } from "react-relay";
 
@@ -6,10 +8,8 @@ import { Header } from "../components/Header";
 import RelayEnvironment from "../lib/RelayEnvironment";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <ReactRelayContext.Provider
-    value={{ environment: RelayEnvironment, variables: {} }}
-  >
-    <div>
+  <ReactRelayContext.Provider value={{ environment: RelayEnvironment }}>
+    <div className="container">
       <Header />
       <Component {...pageProps} />
       <Footer />
