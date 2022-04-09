eb5ae975ef8f3ef97c2c4028483ef741a07a1278
utgwkk <utagawakiki@gmail.com>
2022-04-10 02:04:24 +0900

    ReactRelayContext.Providerで囲む

--- front/pages/_app.tsx
+++ front/pages/_app.tsx
@@ -1,14 +1,20 @@
 import { AppProps } from "next/app";
+import { ReactRelayContext } from "react-relay";
 
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import RelayEnvironment from "../lib/RelayEnvironment";
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <div>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
-  </div>
+  <ReactRelayContext.Provider
+    value={{ environment: RelayEnvironment, variables: {} }}
+  >
+    <div>
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  </ReactRelayContext.Provider>
 );
 
 export default MyApp;
