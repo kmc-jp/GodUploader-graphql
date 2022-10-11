cd6b9a75f0c4bea11f2bc0a3adb10b3c451a4aa9
utgwkk <utagawakiki@gmail.com>
2022-10-11 09:57:13 +0900

    画像アップロードするサンプルコード

---
+++ api/script/example/upload_via_api.py
@@ -0,0 +1,68 @@
+import json
+import os
+import sys
+from pathlib import Path
+from typing import List
+
+import requests
+
+API_BASE_URL = os.environ.get("API_BASE_URL", "http://localhost:5000")
+GRAPHQL_API_ENDPOINT = f"{API_BASE_URL}/api/graphql"
+UPLOAD_ARTWORK_MUTATION = """
+mutation UploadArtworkMutation($input: UploadArtworkInput!) {
+    uploadArtwork(input: $input) {
+        artwork {
+            id
+        }
+    }
+}
+"""
+
+
+def main() -> int:
+    image_path = Path(__file__).parent / "me.png"
+    if not image_path.exists():
+        print(f"File {image_path} not found")
+        return 1
+
+    payload = {
+        "operations": json.dumps(
+            {
+                "query": UPLOAD_ARTWORK_MUTATION,
+                "variables": {
+                    "input": {
+                        "title": "タイトル",
+                        "caption": "説明文です",
+                        "tags": [
+                            "タグ1",
+                            "tag_2",
+                        ],
+                        "rating": "safe",
+                        "shareOption": "SHARE_TO_SLACK_WITH_IMAGE",
+                        "channelId": "C03875433",
+                        "files": [None],
+                    },
+                },
+            }
+        ),
+        "map": json.dumps({"variables.input.files.0": ["variables.input.files.0"]}),
+    }
+    files = {"variables.input.files.0": image_path.open("rb")}
+    headers = {
+        # リファラチェックがあるので渡しておく (:5000 じゃなくて :3000 にしないといけないのであとで調整したい)
+        "Referer": 'http://localhost:3000/',
+    }
+    resp = requests.post(
+        GRAPHQL_API_ENDPOINT, data=payload, headers=headers, files=files
+    )
+
+    print(resp.json())
+
+    if resp.status_code != 200:
+        return 1
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
