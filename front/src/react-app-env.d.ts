a19fc42561f81e33f200172e0b283e3ec82df4f9
utgwkk <utagawakiki@gmail.com>
2023-02-08 00:12:13 +0900

    type

--- front/src/react-app-env.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="react-scripts" />
-// https://github.com/DefinitelyTyped/DefinitelyTyped/issues/35707
-declare module "babel-plugin-relay/macro" {
-  export { graphql } from "react-relay";
-}
