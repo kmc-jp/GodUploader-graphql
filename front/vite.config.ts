2ce7cc55dd52c3d9c11ec022114d0b19cd15beb5
utgwkk <utagawakiki@gmail.com>
2023-02-07 23:41:57 +0900

    node-polyfills

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 import relay from "vite-plugin-relay";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), relay],
+  plugins: [react(), relay, nodePolyfills({ protocolImports: true })],
   // workaround
   define: {
     global: "window",
