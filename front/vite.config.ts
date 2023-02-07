89868dae9ef63f2e362642d470c4658da258a145
utgwkk <utagawakiki@gmail.com>
2023-02-07 23:37:04 +0900

    workaround

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,8 +5,13 @@ import relay from "vite-plugin-relay";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), relay],
+  // workaround
+  define: {
+    global: "window",
+  },
   resolve: {
     alias: {
+      // workaround: https://github.com/danbovey/react-infinite-scroller/issues/253
       "react-infinite-scroller": "react-infinite-scroller/index.js",
     },
   },
