fbe6ea673ca7a66a0a89fb2c89b1c03040f732bb
utgwkk <utagawakiki@gmail.com>
2023-02-07 23:35:57 +0900

    vite-plugin-relay

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import relay from "vite-plugin-relay";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), relay],
   resolve: {
     alias: {
       "react-infinite-scroller": "react-infinite-scroller/index.js",
