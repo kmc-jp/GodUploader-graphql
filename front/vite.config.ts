7a003333e76b7fcbfb9432483e4cf633ed7cee53
utgwkk <utagawakiki@gmail.com>
2023-02-07 23:51:08 +0900

    super workaround

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -5,10 +5,14 @@ import relay from "vite-plugin-relay";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), relay, nodePolyfills({ protocolImports: true })],
+  plugins: [react(), nodePolyfills({ protocolImports: true }), relay],
   // workaround
   define: {
     global: "window",
+    process: undefined,
+    "process.env": (() => {
+      return {};
+    })(),
   },
   resolve: {
     alias: {
