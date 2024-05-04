13c60aac855d7274ce53c5d4aff3a184ee05c256
utgwkk <utagawakiki@gmail.com>
2024-05-04 17:52:46 +0900

    use uwsgi server for production

--- api/script/docker-main.sh
+++ api/script/docker-main.sh
@@ -2,4 +2,8 @@
 set -eu
 
 /app/script/init_db.sh
-poetry run flask run --host 0.0.0.0 --port 5000
+if [ "$USE_DEVELOPMENT_SERVER" = "1" ]; then
+  poetry run flask run --host 0.0.0.0 --port 5000
+else
+  poetry run uwsgi --http 0.0.0.0:5000 --master -p 4 --wsgi-file='src/goduploader/web.py' --callable='app'
+fi
