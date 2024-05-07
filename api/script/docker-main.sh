90013a10be53c067d88424a7836c0357c4431bc1
utgwkk <utagawakiki@gmail.com>
2024-05-07 22:39:58 +0900

    define variable before use

--- api/script/docker-main.sh
+++ api/script/docker-main.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eu
 
+USE_DEVELOPMENT_SERVER=${USE_DEVELOPMENT_SERVER:-0}
+
 /app/script/init_db.sh
 if [ "$USE_DEVELOPMENT_SERVER" = "1" ]; then
   poetry run flask run --host 0.0.0.0 --port 5000
